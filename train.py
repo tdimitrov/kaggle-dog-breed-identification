1a7855ec80c7c9dca4ba1c837ad28086a8b3cb5f
Tsvetomir Dimitrov <tsv.dimitrov@gmail.com>
2018-02-26 10:39:08 +0000

    Reuse last model on training

--- train.py
+++ train.py
@@ -5,6 +5,7 @@ import os
 import sys
 import glob
 from keras.preprocessing.image import ImageDataGenerator
+from keras.models import load_model
 from keras.callbacks import EarlyStopping
 
 
@@ -28,7 +29,13 @@ cv_generator = datagen.flow_from_directory(directory=params.IMAGES_CV_DIR, targe
 early_stop = EarlyStopping(monitor='val_loss', min_delta=0.01, patience=3, verbose=1, mode='auto')
 
 # Combine ResNet50 with the Dense NN
-model = dogs.model.get_model(num_classes)
+if os.path.isfile(params.MODEL_H5):
+    print("Loading %s" % params.MODEL_H5)
+    model = load_model(params.MODEL_H5)
+else:
+    print("Training model from scratch")
+    model = dogs.model.get_model(num_classes)
+
 model.fit_generator(train_generator, steps_per_epoch=num_train//batch_size, epochs=params.EPOCHS, verbose=1, 
                         validation_data=cv_generator, validation_steps=num_cv//batch_size, callbacks=[early_stop])
 
